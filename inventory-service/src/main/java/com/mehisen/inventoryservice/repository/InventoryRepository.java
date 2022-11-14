01305983a4c2dcd8858bfced3a14ea19e3e2ef94
Mahmoud Mehisen <mahmoud.mehisen@gmail.com>
2022-11-15 00:27:39 +0200

    add inventory service

---
+++ inventory-service/src/main/java/com/mehisen/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.mehisen.inventoryservice.repository;
+
+import com.mehisen.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
