ef1607a3f378b22e8c53e936ee32e20183007a2b
Mahmoud Mehisen <mahmoud.mehisen@gmail.com>
2022-11-15 19:18:11 +0200

    create parent

--- order-serivce/src/main/java/com/mehisen/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mehisen.orderservice.repository;
-
-import com.mehisen.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
