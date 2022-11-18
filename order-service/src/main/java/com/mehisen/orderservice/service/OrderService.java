02f71058004990c256cb7d678c65cd759844d3d3
Mahmoud Mehisen <mahmoud.mehisen@gmail.com>
2022-11-18 20:34:46 +0200

    add discorvey

--- order-service/src/main/java/com/mehisen/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/mehisen/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -34,7 +34,7 @@ public class OrderService {
         // Call Inventory Service, and place order if product is in
         // stack
 
-        InventoryResponse[] inventoryResponseArray = webClient.get().uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build()).retrieve().bodyToMono(InventoryResponse[].class).block();
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get().uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build()).retrieve().bodyToMono(InventoryResponse[].class).block();
 
         boolean allProductInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInSock);
 
